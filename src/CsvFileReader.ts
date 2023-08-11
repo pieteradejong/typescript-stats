1874e39033dd9f92bc9475caff6d68013d5ea2b6
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-08-11 18:56:04 -0400

    extract csv reader into separate class

---
+++ src/CsvFileReader.ts
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+export class CsvFileReader {
+    data: string[][] = [];
+    public filename: string;
+    
+    constructor(filename: string) {
+        this.filename = filename;
+    }
+
+    read(): void {
+        this.data = fs.
+        readFileSync(this.filename, {encoding: 'utf-8'})
+        .split('\n')
+        .map((row: string): string[] => {
+            return row.split(',')
+        });
+    }
+}
